40869f517e3168a2957fdccfe82eff4faf006478
Devan Goodwin <dgoodwin@dangerouslyinc.com>
2006-06-25 18:08:42 +0000

    Fixed bug with single occurrence all day events, more code cleanup.

--- src/wuja/feedparser.py
+++ src/wuja/feedparser.py
@@ -6,45 +6,56 @@ import time
 
 from datetime import datetime
 from elementtree import ElementTree
-from model import SingleOccurrenceEntry
-from model import RecurringEntry
 from logging import getLogger
+
+from wuja.model import SingleOccurrenceEntry
+from wuja.model import RecurringEntry
 
 logger = getLogger("feedparser")
 
 class FeedParser:
-    """ Parses the XML provided and returns a list of calendar entries. """
+    """ Parses the XML provided and returns a list of calendar entries.
+    """
 
     def __init__(self, xml):
         self.root_node = ElementTree.XML(xml)
 
     def entries(self):
+        """ Returns the calendar entries for the XML provided. """
         events = []
         for elem in self.root_node.getchildren():
             if parse_tag(elem.tag) == "entry":
                 events.append(create_entry(elem))
         return events
 
-def parse_tag(originalTag):
+def parse_tag(original_tag):
     """ Element Tree tags show up with a {url} prefix. """
-    return originalTag.split("}")[1]
+    return original_tag.split("}")[1]
 
 def parse_timestamp(timestamp):
-    """ Convert internet timestamp (RFC 3339) into a Python datetime object. """
-    # NOTE: Couldn't find anything in the standard python library to parse
-    # these for us:
-    date, time = timestamp.split('T')
-    time, timezone = time.split('.')
-    # NOTE: Not using timezone right now, dates here seem to always come
-    # back GMT.
-    year, month, day = date.split('-')
-    hour, minute, second = time.split(':')
+    """ Convert internet timestamp (RFC 3339) into a Python datetime
+    object.
+    """
+    date_str = None
+    hour = None
+    minute = None
+    second = None
+    # Single occurrence all day events return with only a date:
+    if timestamp.count('T') > 0:
+        date_str, time_str = timestamp.split('T')
+        time_str = time_str.split('.')[0]
+        hour, minute, second = time_str.split(':')
+    else:
+        date_str = timestamp
+        hour, minute, second = 0, 0, 0
+
+    year, month, day = date_str.split('-')
     return datetime(int(year), int(month), int(day), int(hour), int(minute),
         int(second))
 
 def create_entry(elem):
     """ Parses calender entry XML into an Entry object. """
-    id = None
+    entry_id = None
     title = None
     description = None
     updated = None
@@ -54,7 +65,7 @@ def create_entry(elem):
     duration = None
     for node in elem.getchildren():
         if parse_tag(node.tag) == 'id':
-            id = node.text
+            entry_id = node.text
         elif parse_tag(node.tag) == 'title':
             title = node.text
         elif parse_tag(node.tag) == 'content':
@@ -65,15 +76,15 @@ def create_entry(elem):
             recurrence = node.text
         elif parse_tag(node.tag) == 'when':
             when = parse_timestamp(node.attrib["startTime"])
-            endTime = parse_timestamp(node.attrib["endTime"])
-            duration = time.mktime(endTime.timetuple()) - \
+            end_time = parse_timestamp(node.attrib["endTime"])
+            duration = time.mktime(end_time.timetuple()) - \
                 time.mktime(when.timetuple())
         elif parse_tag(node.tag) == 'where':
             where = node.text
-    logger.debug("Found entry: " + title)
+    logger.debug("   Entry: " + title)
     if recurrence != None:
-        return RecurringEntry(id, title, description, where, updated,
+        return RecurringEntry(entry_id, title, description, where, updated,
             recurrence)
-    return SingleOccurrenceEntry(id, title, description, updated, when,
+    return SingleOccurrenceEntry(entry_id, title, description, updated, when,
         duration, "where")
 
