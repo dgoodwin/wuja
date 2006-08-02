5f5e7eddf02692b09aa6bfe7876b03239e3bcc53
Devan Goodwin <dgoodwin@dangerouslyinc.com>
2006-08-02 00:15:33 +0000

    Added thin wrapper script to run the main wuja application from the source directory instead of the installed version.

---
+++ testwuja.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+
+#   Wuja - Google Calendar (tm) notifications for the GNOME desktop.
+#
+#   Copyright (C) 2006 Devan Goodwin <dgoodwin@dangerouslyinc.com>
+#   Copyright (C) 2006 James Bowes <jbowes@dangerouslyinc.com>
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program; if not, write to the Free Software
+#   Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+#   02110-1301  USA
+
+"""
+Thin script to run wuja from the source directory. (and not the installed
+version.
+"""
+
+__revision__ = "$Revision"
+
+import sys
+
+sys.path.insert(0, './src/')
+from wuja.application import WujaApplication
+
+if __name__ == "__main__":
+    wujaApp = WujaApplication()
+    wujaApp.main()
+
